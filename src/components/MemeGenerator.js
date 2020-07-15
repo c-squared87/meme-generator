7f86c283d8ecdf4d61c1aee8faa78aba6612a548
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-07-15 10:50:22 -0500

    initial commit and basic setup

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div className='meme-gen'>MEMS HUR</div>
+        )
+    }
+}
+
+export default MemeGenerator
