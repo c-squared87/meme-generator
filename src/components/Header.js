4302f296cece4c4b5a2cbfc67ec510ec43a90904
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-07-15 11:48:39 -0500

    added api call in MemeGenerator and built controlled form for input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import '../App.css'
 
 function Header() {
     return(
-        <div className='header'>Header Object</div>
+        <div className='header'>
+            <header>
+                <img src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png' alt='probelms?'></img>
+                <p>meme generator</p>
+            </header>
+        </div>
     )
 }
 
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,15 +1,71 @@
 import React from 'react'
-
+import '../App.css'
 class MemeGenerator extends React.Component {
 
     constructor(){
         super()
-        this.state = {}
+        this.state = {
+            topText: '',
+            bottomText: '',
+            randomImage: 'http://i.imgflip.com/1bij.jpg'
+        }
+    }
+
+    componentDidMount(){
+        this.collectImages()
+    }
+
+    collectImages(){
+        const _url = 'https://api.imgflip.com/get_memes'
+        fetch(_url)
+            .then(response => response.json())
+            .then(response => {
+              const {memes} = response.data
+              this.setState({
+                  allMemeImages: memes
+              })
+            })
+    }
+
+    handleTopTextChange = (event) =>{
+        this.setState({
+            topText: event.target.value
+        })
+    }
+
+    handleBottomTextChange = (event) => {
+        this.setState({
+            bottomText: event.target.value
+        })
+    }
+
+    generateMeme(){
+        console.log("fuckin meme time"); 
     }
 
     render(){
         return(
-            <div className='meme-gen'>MEMS HUR</div>
+            <div className='meme'>
+                <form className='meme-form'>
+                    <input
+                        type='text' 
+                        placeholder='top text'
+                        value={this.state.topText}
+                        onChange={this.handleTopTextChange}
+                    />
+                    <input
+                        type='text'
+                        placeholder='bottom text'
+                        value={this.state.bottomText}
+                        onChange={this.handleBottomTextChange}
+                    />
+                    <button onClick={this.generateMeme}>Generate</button>
+                </form>
+
+                <img src={this.state.randomImage} alt="not found"/>
+                <p className='top-text'>{this.state.topText}</p>
+                <p className='bottom-text'>{this.state.bottomText}</p>
+            </div>
         )
     }
 }
